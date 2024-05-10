ffc12bfa042f47d64ea8c24e74d1f7f3ea967b52
vararaghu002 <121346843+vararaghu002@users.noreply.github.com>
2024-05-10 10:29:22 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#element', {
+  strings: ['web developer','data analyser'],
+  typeSpeed: 40,
+});
+function gitlink(){
+  location.href="https://github.com/vararaghu002"
+}
+function resume(){
+   location.href="/resume.pdf"
+}
